2de546a77e9e129247dc32df8b110d1b970e079f
Raul Vaquerizo <rvaqueri@jaumebalmes.net>
2023-03-23 20:48:18 +0100

    ejercicio 1

--- JaumeBalmes_RaulVaquerizo/institut/views.py
+++ JaumeBalmes_RaulVaquerizo/institut/views.py
@@ -9,16 +9,19 @@ def index(request):
 def prof(request):
     prof = {
         "prof1": {
+            "id": "1",
             "name":"Roger",
             "surname":"Sobrino",
             "age":"17"
         },
         "prof2": {
+            "id": "2",
             "name": "Oriol",
             "surname": "Roca",
             "age": "27"
         },
         "prof3": {
+            "id": "3",
             "name": "Jose Javier",
             "surname": "Faro",
             "age": "13"
@@ -32,31 +35,37 @@ def prof(request):
 def alumn(request):
     alumn = {
         "alumn1": {
+            "id": "1",
             "name": "Enric",
             "surname": "Marquez",
             "age": "17"
         },
         "alumn2": {
+            "id": "2",
             "name": "Jaume",
             "surname": "Balmes",
             "age": "27"
         },
         "alumn3": {
+            "id": "3",
             "name": "Pedro",
             "surname": "Gaseoso",
             "age": "13"
         },
         "alumn4": {
+            "id": "4",
             "name": "Jorge",
             "surname": "Rosell",
             "age": "17"
         },
         "alumn5": {
+            "id": "5",
             "name": "Josep",
             "surname": "Enric",
             "age": "27"
         },
         "alumn6": {
+            "id": "6",
             "name": "Raul",
             "surname": "Vaquerizo",
             "age": "13"
