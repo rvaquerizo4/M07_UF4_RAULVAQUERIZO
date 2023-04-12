603100e51cdced60475a28f38fee7311acaa24bd
Raul Vaquerizo <rvaqueri@jaumebalmes.net>
2023-04-12 13:24:45 +0200

    mvt acabada

--- JaumeBalmes_RaulVaquerizo/institut/models.py
+++ JaumeBalmes_RaulVaquerizo/institut/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Alum(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    curso = models.CharField(max_length=50)
+    edad = models.IntegerField()
+    poblacion = models.DateField()
+    graduado = models.BooleanField()
+class Prof(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    curso = models.CharField(max_length=50)
+    edad = models.IntegerField()
+    poblacion = models.DateField()
+    graduado = models.BooleanField()
